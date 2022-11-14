02228e2e8bcac231d3b858d731cbd81fd0680681
Vernon-444 <mt.vernon44@gmail.com>
2022-11-13 22:35:04 -0800

    catching up to prev commit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+Script that starts a Flask web application listening on 0.0.0.0:5000
+Route /: display "Hello HBNB!"
+Route /hbnb: display "HBNB"
+Must use option 'strict_slashes=False'
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_HBNB():
+    """
+    Prints Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """
+    prints HBNB
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+"""
+Script that starts a Flask web application listening on 0.0.0.0:5000
+Route /: display "Hello HBNB!"
+Route /hbnb: display "HBNB"
+Route /c/<text>: display "C" followed by the value of text
+Must use option 'strict_slashes=False'
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_HBNB():
+    """
+    Prints Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """
+    prints HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """
+    prints C followed by value of text
+    """
+    text = text.replace("_", " ")
+    return ("C {}".format(text))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
